037a6e24bc39ea62fe6dc0470099bd42cf8a9f48
Pushkar Deshmukh <pushkard@markk.app>
2022-02-13 19:01:41 +0530

    Create feed item struct

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Admin on 13/02/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
