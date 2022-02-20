fb9115bd5c3935abbd1fcf2f748da057f607e079
Pushkar Deshmukh <pushkard@markk.app>
2022-02-20 09:34:27 +0530

    Fix unit test for iOS14+

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -42,7 +42,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
-                XCTAssertNotNil(receivedError)
+                XCTAssertEqual(receivedError.domain, error.domain)
+                XCTAssertEqual(receivedError.code, error.code)
                 
             default:
                 XCTFail("Expected failure with error \(error), got \(result) instead")
