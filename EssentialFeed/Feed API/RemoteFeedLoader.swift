9767a370b49029b4b1062b9f1e1be50cc2955719
Pushkar Deshmukh <pushkard@markk.app>
2022-03-09 22:01:44 +0530

    Remove references of Items in favour of Images which is a domain term used by domain experts in specs

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -48,8 +48,8 @@ public final class RemoteFeedLoader: FeedLoader {
 }
 
 private extension Array where Element == RemoteFeedItem {
-    func toModels() -> [FeedItem] {
-        return map { FeedItem.init(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image) }
+    func toModels() -> [FeedImage] {
+        return map { FeedImage.init(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
     }
 }
 
