8510d37e3c4b3fd4645ab2f2bbb4f4873845846d
Pushkar Deshmukh <pushkard@markk.app>
2022-06-29 17:09:16 +0530

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Pushkar Deshmukh on 29/06/22.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -8,16 +8,8 @@
 import Foundation
 import EssentialFeed
 
-struct FeedLoadingViewModel {
-    let isLoading: Bool
-}
-
 protocol FeedLoadingView { // only 'class' can be weakly referenced
     func display(_ viewModel: FeedLoadingViewModel)
-}
-
-struct FeedViewModel {
-    let feed: [FeedImage]
 }
 
 protocol FeedView {
