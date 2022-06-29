7279d72393dae465025876a8c90d7cab94473421
Pushkar Deshmukh <pushkard@markk.app>
2022-06-29 17:36:24 +0530

    Move FeedViewController + FeedRefreshController composition (instantiation and configuration) to Storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
-    var refreshController: FeedRefreshViewController?
+    @IBOutlet var refreshController: FeedRefreshViewController?
     
     var tableModel = [FeedImageCellController]() {
         didSet {
@@ -19,9 +19,6 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
-        refreshControl = refreshController?.view
-        
         
         tableView.prefetchDataSource = self
         refreshController?.refresh()
