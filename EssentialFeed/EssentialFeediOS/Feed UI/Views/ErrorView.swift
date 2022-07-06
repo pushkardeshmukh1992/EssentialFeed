5d1f4b69fe05de43a658bfc90b69a30ac1b7afd4
Pushkar Deshmukh <pushkard@markk.app>
2022-07-06 23:31:17 +0530

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Pushkar Deshmukh on 06/07/22.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
