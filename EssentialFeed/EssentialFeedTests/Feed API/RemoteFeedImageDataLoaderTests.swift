47e9d2b8f3da404e510ad3c4c77cf81a7cfb5fad
Pushkar Deshmukh <pushkard@markk.app>
2022-07-11 16:46:48 +0530

    Extract duplicate anyData helper into a shared scope

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -181,15 +181,11 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
     // MARK: Helpers
     
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
-            let client = HTTPClientSpy()
-            let sut = RemoteFeedImageDataLoader(client: client)
-            trackForMemoryLeaks(sut, file: file, line: line)
-            trackForMemoryLeaks(client, file: file, line: line)
-            return (sut, client)
-        }
-    
-    private func anyData() -> Data {
-        return Data("any data".utf8)
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
     }
     
     private func failure(_ error: RemoteFeedImageDataLoader.Error) -> FeedImageDataLoader.Result {
