190cf6451574fc917e274288abf716b73aac3fd4
Pushkar Deshmukh <pushkard@markk.app>
2022-07-13 15:10:24 +0530

    Move FeedImageDataLoaderWithFallbackComposite to production

--- EssentialApp/EssentialApp/FeedImageDataLoaderWithFallbackComposite.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderWithFallbackComposite.swift
@@ -7,33 +7,34 @@
 
 import EssentialFeed
 
-//public class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
-//    private let primary: FeedImageDataLoader
-//    private let fallback: FeedImageDataLoader
-//    
-//    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-//        self.primary = primary
-//        self.fallback = fallback
-//    }
-//    
-//    private class TaskWrapper: FeedImageDataLoaderTask {
-//        var wrapped: FeedImageDataLoaderTask?
-//        
-//        func cancel() {
-//            wrapped?.cancel()
-//        }
-//    }
-//    
-//    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-//        let task = TaskWrapper()
-//        
-//        task.wrapped = primary.loadImageData(from: url, completion: { [weak self] result in
-//            switch result {
-//            case .success: completion(result)
-//            case .failure:
-//                task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
-//            }
-//        })
-//        return task
-//    }
-//}
+class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    private let primary: FeedImageDataLoader
+    private let fallback: FeedImageDataLoader
+    
+    private class TaskWrapper: FeedImageDataLoaderTask {
+        var wrapped: FeedImageDataLoaderTask?
+        
+        func cancel() {
+            wrapped?.cancel()
+        }
+    }
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = TaskWrapper()
+        
+        task.wrapped = primary.loadImageData(from: url, completion: { [weak self] result in
+            switch result {
+            case .success: completion(result)
+                
+            case .failure:
+                task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
+            }
+        })
+        return task
+    }
+}
